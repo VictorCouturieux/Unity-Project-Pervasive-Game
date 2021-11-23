ea17b05fee2d30719208105baac4344a7b13c659
VictorCouturieux <victor.couturieux@laposte.net>
2021-11-23 11:55:30 +0100

    unshow sound volume debug log

--- Assets/Scripts/SoundEffect/FadeMixerGroup.cs
+++ Assets/Scripts/SoundEffect/FadeMixerGroup.cs
@@ -18,7 +18,7 @@ public class FadeMixerGroup : MonoBehaviour
         currentVol = Mathf.Pow(10, currentVol / 20);
         float targetValue = Mathf.Clamp(targetVolume, 0.0001f, 1);
 
-        Debug.Log("currentTime : " + currentTime + " / currentVol : " + currentVol);
+        // Debug.Log("currentTime : " + currentTime + " / currentVol : " + currentVol);
         
         while (currentTime < duration)
         {
